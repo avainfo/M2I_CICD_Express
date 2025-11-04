ef5f304025d6826f700c3dad75b060ffb44222f4
Antonin Do Souto <avainfoconseils@gmail.com>
2025-11-04 10:18:49 +0000

    fix: Correct error in endpoint

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send({"name": "Antonin", "age": undefined});
+    res.send({"name": "Antonin", "age": 21});
 });
 
 module.exports = router;
